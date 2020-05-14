38f06d43a768a92f35b9eda2e7b81775a6d19d2c
dhoman <dhoman@huskers.unl.edu>
2020-05-14 00:44:09 -0600

    notes to myself on things i had to do

--- java/path.zsh
+++ java/path.zsh
@@ -1,4 +1,7 @@
 # will display all the valid versions for setting JAVA_HOME
 # /usr/libexec/java_home -V
-#
+#  i installed these versions of java using homebrew
+# and i had to have 1.8 as the active java before doing
+#   brew install leiningen
+# to get https://github.com/quil/quil/wiki/Installing working
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
