2c96f4d45556b0c9130a7c86b8ebb3b0fd113c4a
Derek Homan <dhoman@huskers.unl.edu>
2020-05-29 21:02:04 -0600

    updated path for android

--- android/path.zsh
+++ android/path.zsh
@@ -1,2 +1,3 @@
 export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
+export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
 export PATH="$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/build-tools:$PATH"
